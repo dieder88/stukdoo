c9157f9dab5090518499a37ce56fdc640e93767f
Robby <robbyd@boldexposure.com>
2016-01-27 11:00:50 -0600

    Added change task custom actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,7 +15,6 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
-
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
